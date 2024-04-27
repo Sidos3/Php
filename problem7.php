32065ab2c5b798c098e71a82943ab9b0d9618a4e
sidahmed <sidahmed@gmail.com>
2024-04-27 18:41:59 +0000

    impliment strlen function

--- problem7.php
+++ problem7.php
@@ -37,3 +37,55 @@ $str = strrev($str);
 $str = ucfirst($str);
 echo $str;
 // Elzero
+echo '<br>';
+echo '<hr>';
+echo '<br>';
+
+$str = 'aAa';
+$num = 3;
+$char = "_";
+
+// Write Your Code Here
+echo str_repeat(chunk_split(strtolower($str), $num, $char), 3);
+// aaa_aaa_aaa_
+echo '<br>';
+echo '<hr>';
+echo '<br>';
+
+$str = "<div><b>Elzero</b></div>";
+echo strip_tags($str,"<b>");
+// <b>Elzero</b>
+
+echo '<br>';
+echo '<hr>';
+echo '<br>';
+
+$str = "Elzero Web School";
+$e = "e";
+$o = "O";
+$four = 4;
+ echo substr_count($str,$e,$four);
+ echo '<br>';
+ echo substr_count($str,strtolower($o),$four,11);
+// 1
+// 2
+echo '<br>';
+echo '<hr>';
+echo '<br>';
+
+$chars = ["E", "l", "z", "e", "r", "o"];
+echo implode($chars);
+// Output
+// "Elzero"
+
+echo '<br>';
+echo '<hr>';
+echo '<br>';
+
+
+$chars = ["E", 1, 2, "l", "z", "E", "R", "o"];
+
+
+
+// Output
+// "Elzero"
